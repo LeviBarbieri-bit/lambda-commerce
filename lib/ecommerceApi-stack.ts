bfed90824c8a8262dd6b58ebc2e8e61ac8d392a7
levibarbieri-bit <levibarbieri@gmail.com>
2024-10-08 20:30:59 -0300

    Implementação de pedidos

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -5,54 +5,91 @@ import * as cwlogs from "aws-cdk-lib/aws-logs"
 import { Construct } from "constructs"
 
 interface ECommerceApiStackProps extends cdk.StackProps {
-  productsFetchHandler: lambdaNodeJS.NodejsFunction,
-  productsAdminHandler: lambdaNodeJS.NodejsFunction
+   productsFetchHandler: lambdaNodeJS.NodejsFunction;
+   productsAdminHandler: lambdaNodeJS.NodejsFunction;
+   ordersHandler: lambdaNodeJS.NodejsFunction;
 }
 
 export class ECommerceApiStack extends cdk.Stack {
 
-  constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
-    super(scope, id, props)
+   constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
+      super(scope, id, props)
 
-    const logGroup = new cwlogs.LogGroup(this, "ECommerceApiLogs")
-    const api = new apigateway.RestApi(this, "ECommerceApi", {
-      restApiName: "ECommerceApi",
-      cloudWatchRole: true,
-      deployOptions: {
-        accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
-        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
-          httpMethod: true,
-          ip: true,
-          protocol: true,
-          requestTime: true,
-          resourcePath: true,
-          responseLength: true,
-          status: true,
-          caller: true,
-          user: true
-        })
-      }
-    })
+      const logGroup = new cwlogs.LogGroup(this, "ECommerceApiLogs")
+      const api = new apigateway.RestApi(this, "ECommerceApi", {
+         restApiName: "ECommerceApi",
+         cloudWatchRole: true,
+         deployOptions: {
+            accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+            accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+               httpMethod: true,
+               ip: true,
+               protocol: true,
+               requestTime: true,
+               resourcePath: true,
+               responseLength: true,
+               status: true,
+               caller: true,
+               user: true
+            })
+         }
+      })
 
-    const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
+      this.createProductsService(props, api)
 
-    // "/products"
-    const productsResource = api.root.addResource("products")
-    productsResource.addMethod("GET", productsFetchIntegration)
+      this.createOrdersService(props, api)
+   }
 
-    // GET /products/{id}
-    const productIdResource = productsResource.addResource("{id}")
-    productIdResource.addMethod("GET", productsFetchIntegration)
+   private createOrdersService(props: ECommerceApiStackProps, api: apigateway.RestApi) {
+      const ordersIntegration = new apigateway.LambdaIntegration(props.ordersHandler)      
 
-    const productsAdminIntegration = new apigateway.LambdaIntegration(props.productsAdminHandler)
+      //resource - /orders
+      const ordersResource = api.root.addResource('orders')
 
-    // POST /products
-    productsResource.addMethod("POST", productsAdminIntegration)
-  
-    // PUT /products/{id}
-    productIdResource.addMethod("PUT", productsAdminIntegration)
+      //GET /orders
+      //GET /orders?email=matilde@siecola.com.br
+      //GET /orders?email=matilde@siecola.com.br&orderId=123
+      ordersResource.addMethod("GET", ordersIntegration)
 
-    // DELETE /products/{id}
-    productIdResource.addMethod("DELETE", productsAdminIntegration)
-  }
+      const orderDeletionValidator = new apigateway.RequestValidator(this, "OrderDeletionValidator", {
+         restApi: api,
+         requestValidatorName: "OrderDeletionValidator",
+         validateRequestParameters: true,
+      })
+
+      //DELETE /orders?email=matilde@siecola.com.br&orderId=123
+      ordersResource.addMethod("DELETE", ordersIntegration, {
+         requestParameters: {
+            'method.request.querystring.email': true,
+            'method.request.querystring.orderId': true
+         },
+         requestValidator: orderDeletionValidator
+      })
+
+      //POST /orders
+      ordersResource.addMethod("POST", ordersIntegration)
+   }
+
+   private createProductsService(props: ECommerceApiStackProps, api: apigateway.RestApi) {
+      const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
+
+      // "/products"
+      const productsResource = api.root.addResource("products")
+      productsResource.addMethod("GET", productsFetchIntegration)
+
+      // GET /products/{id}
+      const productIdResource = productsResource.addResource("{id}")
+      productIdResource.addMethod("GET", productsFetchIntegration)
+
+      const productsAdminIntegration = new apigateway.LambdaIntegration(props.productsAdminHandler)
+
+      // POST /products
+      productsResource.addMethod("POST", productsAdminIntegration)
+
+      // PUT /products/{id}
+      productIdResource.addMethod("PUT", productsAdminIntegration)
+
+      // DELETE /products/{id}
+      productIdResource.addMethod("DELETE", productsAdminIntegration)
+   }
 }
